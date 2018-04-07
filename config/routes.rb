f6bf4edf1e1d426da4017fb120d19580b91e730e
Desmond Pringle <dezmondp@gmail.com>
2018-04-07 06:29:23 -0500

    Integrates friendly routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
-
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
