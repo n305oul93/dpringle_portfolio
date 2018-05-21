a30031f55dacb9e6e48fd849da469222026a8353
D Pringle <dezmondp@gmail.com>
2018-05-20 19:02:21 -0500

    Refactors before action for portfolio setting

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,4 +6,12 @@ class Blog < ApplicationRecord
   validates_presence_of :title, :body
 
   belongs_to :topic, optional: true
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
 end
