74c601f76b11d4974a161032e5dd2f0f7af03bda
D Pringle <dezmondp@gmail.com>
2018-06-23 19:17:38 -0500

    Updates application.rb with env variable for secret key base

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module DesmondPringlePortfolio
     config.load_defaults 5.1
     config.eager_load_paths << Rails.root.join('lib')
     # config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
