48b3191050b06a887f53af282293db8066a535a8
D Pringle <dezmondp@gmail.com>
2018-06-24 00:44:31 -0500

    Integrates production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://djp-portfolio.herokuapp.com', 'http://djp-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://djp-portfolio.herokuapp.com/cable"
 end
